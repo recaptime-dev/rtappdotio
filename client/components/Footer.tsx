910437ca6eef0823aedda9835bbb5b6bff6e0ce7
Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>
2022-04-19 08:58:11 +0000

    Update footer and improve footer Jest tests
    Signed-off-by: Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -26,18 +26,18 @@ const Footer: FC = () => {
     >
       {!isAuthenticated && <ReCaptcha />}
       <Text fontSize={[12, 13]} py={2}>
-        Made with love by developers at{" "}
+        Made with love, originally by developers at{" "}
         <ALink href="https://github.com/thedevs-network/kutt" title="The Devs">
           The Devs
         </ALink>
-        , and improved by{" "}
-        <ALink href="https://madebythepins.tk" title="friends at The Pins Team">
-          The Pins Team
+        , and currently by{" "}
+        <ALink href="https://recaptime.tk" title="friends at Recap Time Squad">
+          Recap Time Squad
         </ALink>
         .{" | "}
         <ALink
-          href="https://github.com/MadeByThePinsHub/rtappdotio"
-          title="Our fork's sauce on GitHub"
+          href="https://github.com/RecapTime/rtappdotio"
+          title="Source on GitHub"
           target="_blank"
         >
           Our fork's sauce on GitHub
